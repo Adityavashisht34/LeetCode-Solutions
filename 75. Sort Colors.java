a2103426ad4c2fb64ac3e650f64eacba1eb3911f
Adityavashisht34 <112467903+Adityavashisht34@users.noreply.github.com>
2023-11-24 14:24:23 +0530

    Create 75. Sort Colors.java
    Link: https://leetcode.com/problems/sort-colors/

---
+++ 75. Sort Colors.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        quickSort(nums, 0, nums.length-1);
+        
+    }
+    int partition(int [] nums, int low, int high){
+        
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+        while(i<j){
+            while(nums[i]<=pivot){
+                i++;
+                if(i==nums.length){
+                    break;
+                }
+            }
+            while(nums[j]>pivot){
+                j--;
+                if(j==-1){
+                    break;
+                }
+            }
+            if(i<j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        int temp = nums[low];
+        nums[low] = nums[j];
+        nums[j] = temp;
+        return j;
+    }
+    void quickSort(int [] nums, int low, int high){
+        if(low<high){
+            int pivot = partition(nums,low,high);
+            quickSort(nums, low, pivot-1);
+            quickSort(nums, pivot+1, high);
+        }
+    }
+}
